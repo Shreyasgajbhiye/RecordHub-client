7175ed5f4ba7c656ec7676a64d0aacfd7b02245f
Shreyasgajbhiye <109300149+Shreyasgajbhiye@users.noreply.github.com>
2024-02-04 00:05:06 +0530

    get mentor

--- src/components/getMentor/mentor.jsx
+++ src/components/getMentor/mentor.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {Link} from 'react-router-dom'
 import './mentor.css'
+import axios from 'axios'
 const Mentor = () => {
+    const [users, setUsers] = useState([]);
+    useEffect(()=>{
+        const fetchData = async() => {
+            const response = await axios.get("http://localhost:8000/api/getAll");
+            setUsers(response.data);
+        }
+        fetchData()
+    },[])
   return (
     <div className="userTable">
-         {/* we cant use html link hence we use "Link from react dom" */}
          
             <Link to={"/add"} className="addButton">Add mentor</Link>  
             <table border={1} cellPadding={10} cellSpacing={0}>
@@ -13,19 +21,27 @@ const Mentor = () => {
                         <th>s. No</th>
                         <th>User name</th>
                         <th>email</th>
+                        <th>Batch</th>
                         <th>Action</th>
                     </tr>
                 </thead>
                     <tbody>
-                        <tr>
-                            <td>1.</td>
-                            <td>Shreyash</td>
-                            <td>s@gmai.com</td>
-                            <td className="actionButton">
-                                <button>Delete</button>
-                                <Link to={"/edit"}>Edit</Link>
-                            </td>
-                        </tr>
+                        {
+                            users.map((user, index)=>{
+                                return(
+                                    <tr key={user._id}>
+                                        <td>{index + 1}</td>
+                                        <td>{user.fname} {user.lname}</td>
+                                        <td>{user.email}</td>
+                                        <td>{user.batch}</td>
+                                        <td className="actionButton">
+                                            <button>Delete</button>
+                                            <Link to={"/edit"}>Edit</Link>
+                                        </td>
+                                    </tr>
+                                )
+                            })
+                        } 
                     </tbody>
             </table>
         </div>
