96ef7586ca837d6f001228e9206a3eac57d3e227
Shreyasgajbhiye <109300149+Shreyasgajbhiye@users.noreply.github.com>
2024-02-07 21:34:35 +0530

    New login ui [Responsive]

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,27 +1,31 @@
 import React from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import COVER_IMG from '../../assets/login.jpg';
+
 const Login = () => {
   return (
-    <div className="login template d-flex justify-content-center align-items-center 100-w vh-100 bg-primary">
-       <div className='form_container p-5 rounded bg-white'>
-        <form>
-            <h1 className='text-center'>Sign In</h1>
-            <div className='mb-2'>
-                <label htmlFor="email">Email</label>
-                <input type="email" placeholder='Enter email' className='form-control'/>
-            </div>
-            <div className='mb-2'>
-                <label htmlFor="password">Password</label>
-                <input type="password" placeholder='Enter password' className='form-control'/>
-            </div>
-            <div className='d-grid'>
-                <button className='btn btn-primary'>Sign In</button>
-            </div>
-            <p>
-                Forgot <a href=''>Password ?</a>
-            </p>
+    <div className='grid grid-cols-1 sm:grid-cols-2 h-screen w-full'>
+      <div className='hidden sm:block'>
+        <img className="w-full h-full object-cover " src={COVER_IMG} alt="" />
+      </div>
+      
+      <div className='bg-gray-100 flex flex-col justify-center'>
+        <form className='max-w-[400px] w-full mx-auto bg-white p-4'>
+          <h2 className='text-4xl font-bold text-center py-6'>Login</h2>
+          <div className='flex flex-col py-2'>
+            <label>Email</label>
+            <input  className='border p-2' type='email'/>
+          </div>
+          <div className='flex flex-col py-2'>
+            <label>Password</label>
+            <input className='border p-2' type='password'/>
+          </div>
+          <button className='border w-full my-5 py-2 bg-indigo-500 hover:bg-indigo-400 text-white'>Login in</button>
+          <div className='flex justify-between'>
+            <p className='flex items-center'><input className='mr-2' type="checkbox"/>Remember me</p>
+          </div>
+          <p className='text-center mt-8'>Don't have account? Create account</p>
         </form>
-       </div>
+      </div>
     </div>
   )
 }
